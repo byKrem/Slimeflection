5f5ef3e992172255ae8e67c919c8bf44adc1ec31
Oleg <bykr3m@gmail.com>
2025-03-27 22:20:16 +0300

    Forgot to add MirrorPoint script

---
+++ mirrorPoint.gd
@@ -0,0 +1,10 @@
+class_name MirrorPoint
+extends Node2D
+
+func _ready() -> void:
+	var distance = $"../Player".global_position - global_position
+
+	$Reflection.global_position = Vector2(
+		global_position.x - distance.x,
+		global_position.y - distance.y
+	)
