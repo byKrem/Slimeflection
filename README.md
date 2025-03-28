a98856fcbe29696107623c7069bea76e85de0fe3
Oleg <bykr3m@gmail.com>
2025-03-28 21:57:21 +0300

    Still working on tutorial It's not done yet

---
+++ README.md
@@ -0,0 +1,7 @@
+# Slimeflection
+You're a tiny slime in this world and you have a friend! Your reflection! 
+
+Try to solve some puzzles to know a little bit more about your friend.
+
+#### Made for Beginner's Jam March 2025
+Work in progress
---
+++ scenes/worlds/Tutorial/hints.gd
@@ -0,0 +1,20 @@
+extends Node
+
+func _ready():
+	$YourNotAloneLabel.visible = false
+	$TryToJumpLabel.visible = false
+	$HelpYourFriendLabel.visible = false
+
+func _on_your_not_alone_body_entered(body:Node2D) -> void:
+	if body:
+		$YourNotAloneLabel.visible = true
+
+
+func _on_try_to_jump_body_entered(body: Node2D) -> void:
+	if body:
+		$TryToJumpLabel.visible = true
+
+
+func _on_help_your_friend_body_entered(body: Node2D) -> void:
+	if body:
+		$HelpYourFriendLabel.visible = true
