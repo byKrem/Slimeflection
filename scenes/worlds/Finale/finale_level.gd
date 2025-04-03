e74f061f10ef86a7b985e34d7f9fded63396b4f0
Oleg <bykr3m@gmail.com>
2025-04-03 16:12:26 +0300

    Finished last level.

--- scenes/worlds/Finale/finale_level.gd
+++ scenes/worlds/Finale/finale_level.gd
@@ -3,6 +3,7 @@ extends Node2D
 @export var default_music : AudioStream
 @export var corrupted_music : AudioStream
 var move_abyss := false
+var time := 0
 
 
 func _ready() -> void:
@@ -13,7 +14,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if move_abyss:
 		var abyss_speed = $Abyss.global_position.distance_to($Player.global_position)/2
-		$Abyss.global_position.x += min(max(abyss_speed,100.0),250) * delta
+		$Abyss.global_position.x += min(max(abyss_speed,60.0),250) * delta
 		$Camera2D.limit_left = $Abyss.global_position.x - 250
 
 
@@ -26,3 +27,27 @@ func _on_abyss_encounter_area_2d_body_exited(body:Node2D) -> void:
 		BgMusic.stream = corrupted_music
 		BgMusic.autoplay = false
 		BgMusic.play()
+
+
+func _on_finish_level_area_2d_body_exited(body:Node2D) -> void:
+	if body:
+		$Abyss.hide()
+		move_abyss = false
+		BgMusic.stop()
+		$Camera2D.global_position = Vector2(6001.0,1762.0)
+		$Player.global_position.x = 6001.0
+		$Player.is_in_drop = true
+		$CanvasLayer/Results.show()
+		$CanvasLayer/Results/Label.text = "Your time:\n" + str(time) + " sec"
+		$DontMoveOnFinish.show()
+
+
+func _on_timer_timeout() -> void:
+	time += 1
+
+
+func _on_tree_exiting() -> void:
+	BgMusic.stop()
+	BgMusic.stream = default_music
+	BgMusic.autoplay = true
+	BgMusic.play()
--- scenes/worlds/Tutorial/tutorial_world.gd
+++ scenes/worlds/Tutorial/tutorial_world.gd
@@ -3,6 +3,9 @@ extends Node2D
 @export var reflection: CharacterBody2D
 
 
+func _ready():
+	$Player.is_in_drop = true
+
 func _on_enable_reflection_area_2_body_entered(body:Node2D) -> void:
 	if body is Player:
 		reflection.visible = !reflection.visible
